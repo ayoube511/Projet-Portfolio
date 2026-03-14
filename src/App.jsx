e51b80a9b5519a95a3db05217ad95ee81b93e98e
ayoub <ayoubmoubssite3@gmail.com>
2026-03-14 15:55:06 +0000

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div style={{ 
+      backgroundColor: "red", 
+      color: "white", 
+      minHeight: "100vh", 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent: "center",
+      fontSize: "3rem",
+      fontWeight: "bold"
+    }}>
+      REACT MARCHE ! 🔥<br />
+      Si tu vois ce texte rouge → on avance
+    </div>
+  )
+}
